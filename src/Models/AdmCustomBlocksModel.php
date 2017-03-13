45912ae9332d67b8877336c1b2df254c7a1eb319
ivinnic <ivinnic@gmail.com>
2017-03-13 16:59:47 +0200

    Add support of custom block from project

--- src/Models/AdmCustomBlocksModel.php
+++ src/Models/AdmCustomBlocksModel.php
@@ -3,43 +3,38 @@
 namespace Qubants\Scholar\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
-use DB;
-use Illuminate\Http\Request;
-use Session;
 
 class AdmCustomBlocksModel extends Model
 {
 
-    //table:
-    public $table_users_settings;
+	public $table_users_settings;
 
-    public $modelConfig;
-    public $modelTables;
-    public $modelColumns;
-    public $modelCrud;
+	public $modelConfig;
+	public $modelTables;
+	public $modelColumns;
+	public $modelCrud;
 
-    public function __construct()
-    {
-        $this->modelConfig = new AdmConfig();
+	public function __construct() {
+		$this->modelConfig = new AdmConfig();
 
-        $this->modelCrud = new AdmCrudModel();
-        $this->modelTables = new DbTablesSettings();
-        $this->modelColumns = new DbColumnsSettings();
-    }
+		$this->modelCrud    = new AdmCrudModel();
+		$this->modelTables  = new DbTablesSettings();
+		$this->modelColumns = new DbColumnsSettings();
+	}
 
-    public function getContentCustomBlock($block_id)
-    {
-        switch ($block_id) {
-            case 24:
-            	$users = DB::table('tbl_user')->select(['id','email','first_name','last_name'])->get();
-                return view('scholar::admin-client.custom.send-email',['users'=>$users])->render();
-                break;
-        }
-    }
-
-
+	public function getContentCustomBlock($block_id) {
+		$custom_blocks = config('courier.custom_blocks');
+		if (!empty($custom_blocks[$block_id])) {
+			$action     = $custom_blocks[$block_id];
+			$class      = explode('@', $action)[0];
+			$method     = explode('@', $action)[1];
+			$app        = app();
+			$controller = $app->make($class);
+			return $controller->callAction($method);
+		} else {
+			return 'Custom block not found:' . $block_id;
+		}
+	}
 }
 
 
--- src/ScholarServiceProvider.php
+++ src/ScholarServiceProvider.php
@@ -19,6 +19,14 @@ class ScholarServiceProvider extends ServiceProvider
 	 * @return void
 	 */
 	public function register() {
+
+		$this->publishes([
+			__DIR__.'/config.php' => config_path('courier.php'),
+		]);
+		$this->mergeConfigFrom(
+			__DIR__.'/config.php', 'courier'
+		);
+
 		if (!file_exists(public_path('admin-constructor'))) {
 			symlink(__DIR__ . '/public', public_path('admin-constructor'));
 		}
