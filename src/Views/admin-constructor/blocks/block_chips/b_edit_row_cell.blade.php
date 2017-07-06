b8e52106c00a44e8b43079faf1e6915a92eeb64f
ivinnic <ivinnic@gmail.com>
2017-07-06 09:58:03 +0300

    Image on create

--- src/Views/admin-constructor/blocks/block_chips/b_edit_row_cell.blade.php
+++ src/Views/admin-constructor/blocks/block_chips/b_edit_row_cell.blade.php
@@ -58,10 +58,18 @@ exit();*/ ?>
 
             @elseif($column['column_display_type']['value']=='image')
 
-
-                @if(isset($row_content))
-                    <img style="max-height:2vw" src="{{$row_content[$column_name]['value']}}">
-                @endif
+                <div style="display: flex;justify-content: flex-start;">
+                    @if(isset($row_content))
+                        <img style="max-height:100%; max-width: 40px; float: left"
+                             src="{{$row_content[$column_name]['value']}}">
+                    @endif
+                        @if($columns[$column_name]['column_inRow_editable']['value']==1)
+                            <input class="unit-save-js form-control" type="text"
+                                   name="{{$row_content[$column_name]['key']}}"
+                                   value="{{$row_content[$column_name]['value']}}"
+                                   style="float: left;">
+                        @endif
+                </div>
 
             @elseif($column['column_display_type']['value']=='checkbox')
 
